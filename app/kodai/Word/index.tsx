d9fd28c8d6cebb3a4454e8d7ccda51860b273877
Kodai <kodai8250@gmail.com>
2025-04-05 20:38:15 +0900

    create word table

--- app/kodai/Word/index.tsx
+++ app/kodai/Word/index.tsx
@@ -1,8 +1,13 @@
-import { Button, TextField } from "@mui/material";
-import { FC, useState } from "react";
+import { Button, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
+import { FC, useCallback, useMemo, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import {translateEnToJa} from "@/app/kodai/actions";
 
+type Word = {
+    number: number
+    english: string
+    japanese: string
+}
 type WordInputs = {
     word: string;
 }
@@ -11,6 +16,7 @@ type WordInputs = {
  * */
 export const Word:FC = () => {
     const [word, setWord] = useState<string>()
+    const [wordRows, setWordRows] = useState<Word[]>()
     const {
         register,
         handleSubmit,
@@ -21,6 +27,17 @@ export const Word:FC = () => {
         setWord(resultText)
         localStorage.setItem(data.word, resultText)
     }
+    const showWordTable = useCallback(() => {
+        setWordRows(
+             Object.keys(localStorage).map((key, index) => {
+                return {
+                    number: index,
+                    english:key,
+                    japanese: localStorage.getItem(key)
+                } as Word
+            })
+        )
+    },[word])
 
     /**
      * hello world with netlify
@@ -36,9 +53,35 @@ export const Word:FC = () => {
         <div>英語と翻訳結果はローカルストレージに保存されます</div>
         <form onSubmit={handleSubmit(onSubmit)}>
             <TextField id="outlined-basic" label="Outlined" variant="outlined" {...register('word')}/>
-            <Button variant="text" type="submit">Text</Button>
+            <Button variant="text" type="submit">翻訳</Button>
         </form>
         <div>{word}</div>
+        <div>
+        <Button onClick={showWordTable}>表示</Button>
+        <Table aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                    <TableCell>Number</TableCell>
+                    <TableCell>English</TableCell>
+                    <TableCell>Japanese</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                    {wordRows && wordRows.map((row) => (
+                        <TableRow
+                            key={row.english}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.number}
+                            </TableCell>
+                            <TableCell>{row.english}</TableCell>
+                            <TableCell>{row.japanese}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
     </div>
 }
 export default Word;
