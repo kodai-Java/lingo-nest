7cd1266359ac386d124ff61614bed598806655dd
Kodai-Java <kodai8250@gmail.com>
2025-04-13 06:19:35 +0900

    added eslint and prettier each settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+export default {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    ...nextCoreWebVitals, // Next.js推奨
+    'plugin:@typescript-eslint/recommended',
+    ...eslintPluginPrettierRecommended, // Prettierと連携
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+}
