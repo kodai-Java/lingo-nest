bfc35ff88313fd6d2e72a2d6342432f14e97808e
Kodai <kodai8250@gmail.com>
2025-04-15 20:26:07 +0900

    簡易的な文字数150文字制限バリデーション実装

--- app/transtory/Transtory/EnglishTranslator/hooks.ts
+++ app/transtory/Transtory/EnglishTranslator/hooks.ts
@@ -5,6 +5,9 @@ import { useJapaneseWordStore } from '../../stores'
 export const useEnglishTranslator = () => {
   const { japaneseWord, updateJapaneseWord } = useJapaneseWordStore((state) => state)
   const onSubmitEnglish: SubmitHandler<EnglishWord> = async (data) => {
+    if (data.englishWord.length > 150) {
+      return;
+    }
     const resultText = await sendEnglishWord(data.englishWord)
     updateJapaneseWord(resultText)
     localStorage.setItem(data.englishWord, resultText)
