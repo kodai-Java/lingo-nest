888b0a2237831f1ec4204eb59b46ef944ba81236
Kodai <kodai8250@gmail.com>
2025-04-16 07:42:13 +0900

    added empty validation

--- app/transtory/Transtory/EnglishTranslator/hooks.ts
+++ app/transtory/Transtory/EnglishTranslator/hooks.ts
@@ -5,7 +5,8 @@ import { useJapaneseWordStore } from '../../stores'
 export const useEnglishTranslator = () => {
   const { japaneseWord, updateJapaneseWord } = useJapaneseWordStore((state) => state)
   const onSubmitEnglish: SubmitHandler<EnglishWord> = async (data) => {
-    if (data.englishWord.length > 150) {
+    //TODO 仮でバリデーション設置 zodに置き換え
+    if (!data.englishWord || data.englishWord.length > 150) {
       return;
     }
     const resultText = await sendEnglishWord(data.englishWord)
