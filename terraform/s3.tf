c60d74a8070b074f28b2e386941866a40fd6bfe5
Kodai-Java <kodai8250@gmail.com>
2025-05-04 21:57:53 +0900

    saving tf file

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,7 @@
-resource "aws_s3_bucket" "lingo_bucket" {
+resource "aws_s3_bucket" "lingo_nest_bucket" {
   bucket = "lingo-nest.com"
+  tags = {
+    Name = "LingoNest Static Site"
+    Environment = "prod"
+  }
 }
