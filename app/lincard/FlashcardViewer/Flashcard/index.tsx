49ebc3b444a4b3ddbc7afe89f47867e8d7aded15
Kodai-Java <kodai8250@gmail.com>
2025-05-06 07:06:16 +0900

    created card

--- app/lincard/FlashcardViewer/Flashcard/index.tsx
+++ app/lincard/FlashcardViewer/Flashcard/index.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 type FlashcardProps = {}
 /**
  * 表面(英語)と裏面(日本語)を切り替えて表示
  */
 export const Flashcard: React.FC<FlashcardProps> = ({}) => {
-  return <></>
+  return (
+    <div className="w-full max-w-xl p-8 bg-white rounded-2xl border border-gray-200 shadow-md transition-all duration-300 mx-auto my-10">
+      <p className="text-2xl font-semibold text-gray-900 mb-6 text-center"></p>
+    </div>
+  )
 }
