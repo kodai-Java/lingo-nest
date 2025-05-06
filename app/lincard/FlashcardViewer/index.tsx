8715fa0e92aa38430188d69409620e9c01232b1e
Kodai-Java <kodai8250@gmail.com>
2025-05-07 06:30:30 +0900

    deleted temporary buttons

--- app/lincard/FlashcardViewer/index.tsx
+++ app/lincard/FlashcardViewer/index.tsx
@@ -11,15 +11,6 @@ type FlashcardViewerProps = {}
 export const FlashcardViewer: React.FC<FlashcardViewerProps> = ({}) => {
   return (
     <div>
-      <AnswerButton className="bg-green-50 text-green-700 border-green-200 hover:bg-green-100">
-        覚えた
-      </AnswerButton>
-      <AnswerButton className="bg-red-50 text-red-700 border-red-200 hover:bg-red-100">
-        覚えてない
-      </AnswerButton>
-      <AnswerButton className="bg-blue-50 text-blue-700 border-blue-200 hover:bg-blue-100">
-        答えを見る
-      </AnswerButton>
       <Flashcard />
     </div>
   )
