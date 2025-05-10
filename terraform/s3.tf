d0fc8826e599ad4519ea9b0375f34b3fd4e473a0
Kodai-Java <kodai8250@gmail.com>
2025-05-11 07:16:34 +0900

    created s3 tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,32 @@
 resource "aws_s3_bucket" "lingo_nest_bucket" {
   bucket = "lingo-nest.com"
-  tags = {
-    Name = "LingoNest Static Site"
-    Environment = "prod"
-  }
 }
+
+# data "aws_caller_identity" "current" {}
+
+# data "aws_cloudfront_distribution" "lingo_nest_distribution" {
+#   id = "E2T8YE94PF6IZN"
+# }
+# resource "aws_s3_bucket_policy" "lingo_nest_bucket_policy" {
+#   bucket = aws_s3_bucket.lingo_nest_bucket.id
+#   policy = jsonencode({
+#     Version = "2008-10-17"
+#     Id      = "PolicyForCloudFrontPrivateContent"
+#     Statement = [
+#       {
+#         Sid    = "AllowCloudFrontServicePrincipal"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "cloudfront.amazonaws.com"
+#         }
+#         Action   = "s3:GetObject"
+#         Resource = "${aws_s3_bucket.lingo_nest_bucket.arn}/*"
+#         Condition = {
+#           StringEquals = {
+#             "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.lingo_nest_distribution.id}"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
