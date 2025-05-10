a7d1c5bd554167245e0639e18dfe9e158af9cdb3
Kodai-Java <kodai8250@gmail.com>
2025-05-11 07:50:41 +0900

    added s3 bucket policy

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,31 +2,26 @@ resource "aws_s3_bucket" "lingo_nest_bucket" {
   bucket = "lingo-nest.com"
 }
 
-# data "aws_caller_identity" "current" {}
-
-# data "aws_cloudfront_distribution" "lingo_nest_distribution" {
-#   id = "E2T8YE94PF6IZN"
-# }
-# resource "aws_s3_bucket_policy" "lingo_nest_bucket_policy" {
-#   bucket = aws_s3_bucket.lingo_nest_bucket.id
-#   policy = jsonencode({
-#     Version = "2008-10-17"
-#     Id      = "PolicyForCloudFrontPrivateContent"
-#     Statement = [
-#       {
-#         Sid    = "AllowCloudFrontServicePrincipal"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "cloudfront.amazonaws.com"
-#         }
-#         Action   = "s3:GetObject"
-#         Resource = "${aws_s3_bucket.lingo_nest_bucket.arn}/*"
-#         Condition = {
-#           StringEquals = {
-#             "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.lingo_nest_distribution.id}"
-#           }
-#         }
-#       }
-#     ]
-#   })
-# }
+resource "aws_s3_bucket_policy" "lingo_nest_bucket_policy" {
+  bucket = aws_s3_bucket.lingo_nest_bucket.id
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Id      = "PolicyForCloudFrontPrivateContent"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.lingo_nest_bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::975049915902:distribution/E2T8YE94PF6IZN"
+          }
+        }
+      }
+    ]
+  })
+}
