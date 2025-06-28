596dbb6890aabfd03a2922a1445c9d100ea71c6f
Kodai-Java <kodai8250@gmail.com>
2025-06-28 16:35:36 +0900

    静的ページにしない

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   trailingSlash: true, // CloudFrontとS3の互換性のためにURLと同じフォルダ構造でエクスポート
-  output: 'export', // CloudFrontとS3構成にするために静的エクスポート
-  /* config options here */
+  // output: 'export', // CloudFrontとS3構成にするために静的エクスポート
+  /* config optikns here */
 }
-
 export default nextConfig
